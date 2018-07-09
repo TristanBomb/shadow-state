98f008a0ca5417cc0bbc424efe824f657ad937d9
TristanBomb <TristanBomb@gmail.com>
2018-07-08 17:07:12 -0700

    Began work on documentation

--- src/modifiable.rb
+++ src/modifiable.rb
@@ -1,27 +1,60 @@
+require 'set'
+require_relative 'modifier'
+require_relative 'world'
+
+# A game object that can have modifiers, statistics, and dynamic variables.
+# @note Modifiable is intended for inheritance, rather than direct use.
 class Modifiable
+  # @return [World] the world this object belongs to
   attr_reader :world
+  # @return [Hash{Symbol=>Numeric}] a table of static values for this object
+  # @note Stats are not designed to be modified directly; use modifiers instead
   attr_reader :stats
+  # @return [Hash{Symbol=>Numeric}] a table of dynamic values for this object
+  # @note Vars can't be changed with modifiers; do this directly instead
   attr_accessor :vars
+  # @return [Set<Modifier>] the set of modifiers that apply to this object
   attr_accessor :modifiers
 
+  # The default vars for this object.
   @@defaults = nil
 
+  # Creates a new modifiable object.
+  #
+  # @param world [World] the world to create this object in
+  # @return [self] the newly-created object
   def initialize(world)
     @world = world
     @stats = @@defaults.stats
     @vars = @@defaults.vars
     @modifiers = Set[]
+    self
   end
 
+  # Takes actions that occur on the monthly tick, including modifier ticks and updating stats.
+  #
+  # @return [self]
   def monthly_tick
     @modifiers.each { |mod| mod.monthly_tick }
     update_stats
+    self
   end
 
+  # Takes actions that occur on the yearly tick, including modifier ticks and updating stats.
+  #
+  # @return [self]
   def yearly_tick
     @modifiers.each { |mod| mod.yearly_tick }
+    update_stats
+    self
   end
 
+  # Adds a new modifier to this object.
+  #
+  # @param mod_class [Class] the class to base the new modifier off
+  # @param override [Type] whether to remove any other modifiers with the same tag
+  # @param date [Date] the current date
+  # @return [self]
   def apply_modifier(mod_class, override: false, date: nil)
     collisions = @modifiers.select{|m| m.class == mod_class}
     if collisions.any? && override then
@@ -39,19 +72,34 @@ class Modifiable
       update_stats
       true
     end
+    self
   end
 
+  # Resets the +vars+ of this object based on its +stats+.
+  #
+  # @return [Hash{Symbol=>Numeric}] the new +vars+
   def reset_vars
     @vars = @@defaults.stats2vars(@stats)
   end
 
+  # Updates the +stats+ of this object based on its modifiers.
+  #
+  # @return [Hash{Symbol=>Numeric}] the new +stats+
   def update_stats
     @stats = @@defaults.stats
     @modifiers.each do |mod|
       stats.merge!(mod.effects) { |key, v1, v2| v1 + v2 }
     end
+    @stats
   end
 
+  # If a method does not exist on the object, and the method name is a key in vars or stats,
+  # you can use method syntax to access those. Vars gets first priority, then vars=, then stats.
+  #
+  # @param sym [Symbol] the key to access
+  # @param args [Type] what to set the key equal to (vars= only)
+  # @return [Numeric] the value corresponding to the key
+  # @note Accessing a missing method will still raise the appropriate exception if the key cannot be found!
   def method_missing(sym, *args)
     if @vars.key?(sym)
       @vars[sym]
@@ -59,8 +107,6 @@ class Modifiable
       @vars[sym] = *args
     elsif @stats.key?(sym)
       @stats[sym]
-    elsif @stats.key?(sym.to_s.chomp('=').to_sym)
-      @stats[sym] = *args
     else
       super
     end
--- src/province.rb
+++ src/province.rb
@@ -1,13 +1,27 @@
+require_relative 'modifiable'
+
+# A province in the game world
 class Province < Modifiable
+  # @return [Symbol] a unique identifier for this province (eg. +:california+)
   attr_reader :tag
+  # @return [Country] the country that owns this province
   attr_accessor :owner
+  # @return [Country] the country that occupies this province; same as +owner+ in peacetime
   attr_accessor :occupant
+  # @return [Numeric] the population value of this province; this property is _not_ linear
   attr_accessor :population
+  # @return [Numeric] the development value of this province; this property is _not_ linear
   attr_accessor :development
+  # @return [Numeric] this province's progress towards gaining another population value
   attr_accessor :population_progress
+  # @return [Numeric] this province's progress towards gaining another development value
   attr_accessor :development_progress
+  # @return [Hash{Country=>Symbol}] a hash containing different countries' claims on this province
+  #   The symbols can have the following values: +:conquest+, +:reconquest+, +:unification+
   attr_accessor :claims
 
   # Temporary place to put equations:
   # Economic output = @population * @development * exp(sum_of_modifiers)
+  # Increase in dev progress = economic output * (1-tax_rate)
+  # Development progress required = @population * @development * 240
 end
